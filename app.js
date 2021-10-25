688e232c889e5ed8c8afbec5636d678845035b45
Abdullah Alrayes <abdullah.a.abas@gmail.com>
2021-10-25 16:25:51 +0300

    fsineihd

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
+const { request } = require('express');
 const express = require('express');
 const data = require('./data');
 
 const app = express();
+app.use(express.json());
 const port = 8000;
+app.listen(port, () => {
+  console.log('Hi test');
+});
 
 app.get('/', (req, res) => {
   res.json(data);
 });
 
-app.listen(port, () => {
-  console.log('Hi');
+app.post('/api/products', (req, res) => {
+  const body = req.body;
+  res.statusCode = 201;
+  res.json(body);
+});
+
+app.delete('/api/products/:productId', (req, res) => {
+  const id = +req.params.productId;
+  const isExist = data.find((item) => item.id === id);
+  if (isExist) {
+    res.statusCode = 204;
+    const updatedData = data.filter(
+      (item) => item.id !== +req.params.productId
+    );
+    res.json(updatedData);
+  } else {
+    res.statusCode = 404;
+    res.json({
+      message: 'item not exisit',
+    });
+  }
 });
